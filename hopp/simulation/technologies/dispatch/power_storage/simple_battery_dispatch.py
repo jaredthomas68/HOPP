763eaf6ca2a230090359f88411630874b19c59f8
Jared Thomas <jaredthomas68@gmail.com>
2025-04-01 09:05:58 -0600

    include updates to simple_battery_dispatch

--- hopp/simulation/technologies/dispatch/power_storage/simple_battery_dispatch.py
+++ hopp/simulation/technologies/dispatch/power_storage/simple_battery_dispatch.py
@@ -1,13 +1,14 @@
 import pyomo.environ as pyomo
 from pyomo.environ import units as u
 
-import PySAM.BatteryStateful as BatteryModel
+import PySAM.BatteryStateful as PySAMBatteryModel
 
 from hopp.simulation.technologies.dispatch.power_storage.power_storage_dispatch import (
     PowerStorageDispatch,
 )
 from hopp.simulation.technologies.financial import FinancialModelType
 
+from typing import Union
 
 class SimpleBatteryDispatch(PowerStorageDispatch):
     """A dispatch class for simple battery operations."""
@@ -16,7 +17,7 @@ class SimpleBatteryDispatch(PowerStorageDispatch):
         self,
         pyomo_model: pyomo.ConcreteModel,
         index_set: pyomo.Set,
-        system_model: BatteryModel.BatteryStateful,
+        system_model: PySAMBatteryModel.BatteryStateful,
         financial_model: FinancialModelType,
         block_set_name: str,
         dispatch_options,
@@ -26,7 +27,7 @@ class SimpleBatteryDispatch(PowerStorageDispatch):
         Args:
             pyomo_model (pyomo.ConcreteModel): The Pyomo model instance.
             index_set (pyomo.Set): The Pyomo index set.
-            system_model (BatteryModel.BatteryStateful): The battery stateful model.
+            system_model (PySAMBatteryModel.BatteryStateful): The battery stateful model.
             financial_model (FinancialModelType): The financial model type.
             block_set_name (str): Name of the block set.
             dispatch_options: Dispatch options.
@@ -68,7 +69,8 @@ class SimpleBatteryDispatch(PowerStorageDispatch):
 
     def _set_control_mode(self):
         """Sets control mode."""
-        if isinstance(self._system_model, BatteryModel.BatteryStateful):
+        from hopp.simulation.technologies.ldes.ldes_system_model import LDES as LDESStateful #TODO is there a more elegant way to handle this type check without a circular import?
+        if isinstance(self._system_model, Union[PySAMBatteryModel.BatteryStateful, LDESStateful]):
             self._system_model.value("control_mode", 1.0)  # Power control
             self._system_model.value("input_power", 0.0)
             self.control_variable = "input_power"
